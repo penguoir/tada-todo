c55406a8f57431c2d36c007d21c3086e95eaa7b2
penguoir <games@marash.net>
2018-11-12 22:04:41 +0000

    Fix #6

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,11 +34,11 @@ class Index extends React.Component {
     var query = project
       ? firestore
           .collection('tasks')
-          .where('user', '==', userid)
           .where('project', '==', project[1])
+          .orderBy('timestamp')
       : firestore
           .collection('tasks')
-          .where('user', '==', userid)
+          .orderBy('timestamp')
     
     // Preform the query
     query.onSnapshot(doc => {
